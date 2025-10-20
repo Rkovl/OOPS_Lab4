b9ef23de33f4ef8aaed171ce599264a626b55f00
Ralph Juliano <ralph.juliano@myokanagan.bc.ca>
2025-10-20 14:38:22 -0700

    Create Customer.java

---
+++ src/Customer.java
@@ -0,0 +1,22 @@
+package restaurant;
+// Customer class
+public class Customer {
+    private String name; // initializing private variables of name and order
+    private Order order;
+
+// Initializing the constructor for the Customer class
+    public Customer(String name) {
+        this.name = name;
+        this.order = new Order();  
+    }
+
+    public void addToOrder(MenuItem item) {  // changed addMenuItemToOrder to addToOrder
+        order.addItem(item);  // a method that takes in an instance of MenuItem item, then add it to the order
+    }
+
+    public void placeOrder() {
+        System.out.println("Customer: " + name);
+        order.printOrderDetails(); // a method that prints in the order details of the Customer
+    }
+}
+
