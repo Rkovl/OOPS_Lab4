cb6ca01294763603fa5fa59d58f67c2a92f6cd5a
Ralph Juliano <ralph.juliano@myokanagan.bc.ca>
2025-10-20 14:17:50 -0700

    Update Customer.java

--- src/Customer.java
+++ src/Customer.java
@@ -1,20 +1,21 @@
 package restaurant;
 // Customer class
 public class Customer {
-    private String name;
+    private String name; // initializing private variables of name and order
     private Order order;
 
+// Initializing the constructor for the Customer class
     public Customer(String name) {
         this.name = name;
-        this.order = new Order();  // Collaborating with Order class
+        this.order = new Order();  
     }
 
-    public void addToOrder(MenuItem item) {
-        order.addItem(item);  // Collaborating with Order to add items
+    public void addToOrder(MenuItem item) { 
+        order.addItem(item);  // a method that takes in an instance of MenuItem item, then add it to the order
     }
 
     public void placeOrder() {
         System.out.println("Customer: " + name);
-        order.printOrderDetails();  // Collaborating with Order to display order details
+        order.printOrderDetails(); // a method that prints in the order details of the Customer
     }
 }
